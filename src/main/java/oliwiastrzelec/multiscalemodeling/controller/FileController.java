724e9f45c94d80ef5acb09b978cb4f97598c0cd8
OliwiaStrzelec <oliwiastrzelec@interia.eu>
2018-11-09 11:27:10 +0100

    added bitmap export

--- src/main/java/oliwiastrzelec/multiscalemodeling/controller/FileController.java
+++ src/main/java/oliwiastrzelec/multiscalemodeling/controller/FileController.java
@@ -5,12 +5,11 @@ import oliwiastrzelec.multiscalemodeling.model.MultiscaleModelHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
 @Controller
 public class FileController {
@@ -22,5 +21,21 @@ public class FileController {
         response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
         MultiscaleModel instance = MultiscaleModel.getInstance();
         return MultiscaleModelHelper.getDataFile(instance.getArray(), instance.getSizeX(), instance.getSizeY());
+    }
+
+    @ResponseBody
+    @PostMapping("/exportBitmap")
+    public void getFile(HttpServletResponse response) {
+        try {
+            String fileName = "bitmap.bmp";
+            File file = new File("src/main/resources/static/bitmap.bmp");
+            response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
+            MultiscaleModelHelper.getBitmap(MultiscaleModel.getInstance().getArray());
+            InputStream is = new BufferedInputStream(new FileInputStream(file));
+            org.apache.commons.io.IOUtils.copy(is, response.getOutputStream());
+            response.flushBuffer();
+        } catch (IOException ex) {
+            throw new RuntimeException("IOError writing file to output stream");
+        }
     }
 }
